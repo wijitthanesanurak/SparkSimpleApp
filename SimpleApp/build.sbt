c2fd5bfc0d8ca9262c6b73d32b7a35b0c5158c68
wijit <wijit@DESKTOP-2BT890K>
2017-09-17 21:47:10 +0700

    First commit

---
+++ SimpleApp/build.sbt
@@ -0,0 +1,5 @@
+name := "SimpleApp"
+version := "1.1"
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
---
+++ SimpleApp/src/main/scala/SimpleApp.scala
@@ -0,0 +1,18 @@
+package com.trueit.examples
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "C:/spark-2.2.0-bin-hadoop2.7/README.md" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line=>line.contains("a")).count()
+    val numBs = logData.filter(line=>line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    
+  }
+}
