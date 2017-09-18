bdeda10c585c2a5ed416a6dee45e71ba791cce4c
wijit <wijit@DESKTOP-2BT890K>
2017-09-18 09:40:53 +0700

    test

--- SimpleApp/src/main/scala/SimpleApp.scala
+++ SimpleApp/src/main/scala/SimpleApp.scala
@@ -14,6 +14,7 @@ object SimpleApp {
     val numBs = logData.filter(line=>line.contains("b")).count()
     
     
+    
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
     
   }
